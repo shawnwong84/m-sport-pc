cf3834b244fd82f4d4e09b7f315c30b19dab7911
stock <1378538318@qq.com>
2021-12-15 23:00:51 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
     root: true,
     env: {
         browser: true,
         es6: true,
-        node: true,
     },
-    extends: ['eslint:recommended'],
+    extends: ['eslint:recommended', 'plugin:vue/essential'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
     parserOptions: {
         parser: 'babel-eslint',
         ecmaVersion: 2018,
@@ -15,10 +16,6 @@ module.exports = {
     },
     plugins: ['vue'],
     rules: {
-        // allow debugger during development
-        'vue/no-use-v-if-with-v-for': 'off',
-        'require-v-for-key': 'off',
-        'no-unused-vars': 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        
     },
 };
