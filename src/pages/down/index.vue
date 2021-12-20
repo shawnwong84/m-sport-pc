503ebcd5cbf93456e45f7546440cb07f2f321472
stock <1378538318@qq.com>
2021-12-20 13:36:27 +0800

    down 下载页面样式

--- src/pages/down/index.vue
+++ src/pages/down/index.vue
@@ -7,12 +7,34 @@
             <div></div>
         </div>
         <div class="down-right">
-    
+            <div></div>
+            <h3 class="down-des">体育直播移动下载 - 畅想高清比赛</h3>
+            <div class="down-qr">
+                <vue-qr
+                    text="http://facebook.github.io/react/"
+                    :size="190"
+                    :margin="24"
+                />
+            </div>
+            <div class="down-btn">
+                <div class="ios-btn">
+                    <img src="../../assets/image/down/ios-icon.png" alt="" />
+                    <span>IOS下载</span>
+                </div>
+                <div class="android-btn">
+                    <img
+                        src="../../assets/image/down/android-icon.png"
+                        alt=""
+                    />
+                    <span>安卓下载</span>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import VueQr from 'vue-qr';
 export default {
     name: 'index',
     data() {
@@ -29,6 +51,9 @@ export default {
                 this.ios = res.data.ios;
             }
         });
+    },
+    components: {
+        VueQr,
     },
     methods: {
         down(type) {
@@ -67,10 +92,49 @@ export default {
             }
         }
     }
-    .down-right{
+    .down-right {
         width: 800px;
         height: 800px;
-       
+        .down-des {
+            color: #605656;
+            font-size: 38px;
+        }
+        .down-qr {
+        }
+        .down-btn {
+            width: 100%;
+            @include flexStartCenter();
+            .ios-btn {
+                width: 242px;
+                height: 63px;
+                @include bgurl('../../assets/image/down/ios-btn.png');
+                @include flexCenter();
+                img {
+                    width: 38px;
+                    height: 38px;
+                    transform: translateX(-50px);
+                }
+                span {
+                    color: #ffffff;
+                    font-size: 20px;
+                }
+            }
+            .android-btn {
+                width: 242px;
+                height: 63px;
+                @include bgurl('../../assets/image/down/android-btn.png');
+                @include flexCenter();
+                img {
+                    width: 38px;
+                    height: 38px;
+                    transform: translateX(-50px);
+                }
+                span {
+                    color: #ffffff;
+                    font-size: 20px;
+                }
+            }
+        }
     }
 }
 </style>
