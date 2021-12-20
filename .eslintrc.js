fa1d080aaec1fdf4bbd0e2bc0c6834a697ebd0da
stock <1378538318@qq.com>
2021-12-20 14:03:00 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,11 @@
 module.exports = {
     root: true,
     env: {
-        browser: true,
-        es6: true,
+        node: true,
     },
-    extends: ['eslint:recommended', 'plugin:vue/essential'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
+    extends: ['plugin:vue/essential', 'eslint:recommended'],
     parserOptions: {
         parser: 'babel-eslint',
-        ecmaVersion: 2018,
-        sourceType: 'module',
     },
-    plugins: ['vue'],
-    rules: {
-        // 'no-undef':'off',
-        // 'no-unused-vars' :'off',
-        // 'vue/no-use-v-if-with-v-for' :'off',
-        // 'no-dupe-keys' :'off',
-        // 'vue/require-v-for-key':'off',
-        // 'vue/no-unused-vars':'off'
-    },
+    rules: {},
 };
