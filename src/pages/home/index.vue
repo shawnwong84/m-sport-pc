170ef95f457da4935b4f153a57ee912d7398e99f
stock <1378538318@qq.com>
2021-12-21 18:49:25 +0800

    1

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1982,7 +1982,7 @@ export default {
 .go-room-btn {
     position: absolute;
     left: 50%;
-    transform: translateY(-50%, -50%);
+    transform: translate(-50%, -50%);
     top: 50%;
     z-index: 9;
     width: 130px;
