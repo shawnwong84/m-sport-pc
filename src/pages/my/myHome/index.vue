0e0106a83e03fc7ced576f496f8b6dfe47ae32ca
full <zj15355208214@gmail.com>
2021-12-29 17:21:29 +0800

    我的首页修改

--- src/pages/my/myHome/index.vue
+++ src/pages/my/myHome/index.vue
@@ -53,7 +53,9 @@
                                 <span class="title">我的手机</span>
                                 <span class="des">已经绑定手机号码</span>
                             </div>
-                            <div class="change-btn">更换手机</div>
+                            <div class="change-btn" @click="openPhoneDialog">
+                                更换手机
+                            </div>
                         </div>
                         <div class="change-item">
                             <div class="change-icon">
@@ -66,12 +68,90 @@
                                 <span class="title">我的密码</span>
                                 <span class="des">已经设置密码</span>
                             </div>
-                            <div class="change-btn">更换密码</div>
+                            <div class="change-btn" @click="openPasswordDialog">
+                                更换密码
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <el-dialog
+            title="更换手机"
+            :visible.sync="phoneVisible"
+            width="600px"
+            :close-on-click-modal="false"
+        >
+            <div class="form-box">
+                <div class="form-item">
+                    <span class="form-label">手机号：</span>
+                    <el-input v-model="getUserInfo.phone" disabled></el-input>
+                </div>
+                <div class="form-item">
+                    <span class="form-label">验证码：</span>
+                    <el-input
+                        placeholder="请输入验证码"
+                        v-model="smsCode"
+                        maxlength="6"
+                    ></el-input>
+                    <el-button
+                        type="primary"
+                        class="sms-btn"
+                        @click="sendSms"
+                        >{{ codeMsg }}</el-button
+                    >
+                </div>
+                <div class="form-item">
+                    <el-button
+                        type="primary"
+                        class="sub-btn"
+                        @click="updatePhoneNd"
+                    >
+                        确定
+                    </el-button>
+                </div>
+            </div>
+        </el-dialog>
+        <el-dialog
+            title="更换密码"
+            :visible.sync="passwordVisible"
+            width="600px"
+            :close-on-click-modal="false"
+        >
+            <div class="form-box">
+                <div class="form-item">
+                    <span class="form-label">手机号：</span>
+                    <el-input v-model="getUserInfo.phone" disabled></el-input>
+                </div>
+                <div class="form-item">
+                    <span class="form-label">验证码：</span>
+                    <el-input
+                        placeholder="请输入验证码"
+                        v-model="smsCode"
+                        maxlength="6"
+                    ></el-input>
+                    <el-button
+                        type="primary"
+                        class="sms-btn"
+                        @click="sendSms"
+                        >{{ codeMsg }}</el-button
+                    >
+                </div>
+                <div class="form-item">
+                    <span class="form-label">密码：</span>
+                    <el-input v-model="password" show-password></el-input>
+                </div>
+                <div class="form-item">
+                    <el-button
+                        type="primary"
+                        class="sub-btn"
+                        @click="forgotPassword"
+                    >
+                        确定
+                    </el-button>
+                </div>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -81,7 +161,14 @@ import ProgressBar from 'vue-simple-progress';
 export default {
     name: 'myHome',
     data() {
-        return {};
+        return {
+            phoneVisible: false,
+            passwordVisible: false,
+            codeMsg: '获取验证码',
+            countdown: 60,
+            smsCode: '',
+            password: '',
+        };
     },
     components: {
         ProgressBar,
@@ -96,6 +183,132 @@ export default {
     methods: {
         toPage(path) {
             this.$router.push(path);
+        },
+        openPhoneDialog() {
+            this.phoneVisible = !this.phoneVisible;
+            this.timer = null;
+            this.countdown = 60;
+            this.smsCode = '';
+        },
+        openPasswordDialog() {
+            this.passwordVisible = !this.passwordVisible;
+            this.timer = null;
+            this.countdown = 60;
+            this.smsCode = '';
+        },
+        // 发送验证码
+        sendSms() {
+            if (!this.timer) {
+                let param = {
+                    phone: this.getUserInfo.phone,
+                };
+                this.$axios('post', '/user/sendSms', param).then((res) => {
+                    if (res.code === 200) {
+                        this.$message({
+                            type: 'success',
+                            message: '发送验证码成功',
+                        });
+                        this.getCode();
+                    } else {
+                        this.$message({
+                            type: 'error',
+                            message: res.msg,
+                        });
+                    }
+                });
+            }
+        },
+        // 获取验证码
+        getCode() {
+            this.timer = setInterval(() => {
+                if (this.countdown > 0 && this.countdown <= 60) {
+                    this.countdown--;
+                    if (this.countdown !== 0) {
+                        this.codeMsg = this.countdown + 'S';
+                    } else {
+                        clearInterval(this.timer);
+                        this.codeMsg = '获取验证码';
+                        this.countdown = 60;
+                        this.timer = null;
+                    }
+                }
+            }, 1000);
+        },
+        // 绑定手机
+        updatePhoneNd() {
+            if (this.smsCode === '') {
+                this.$message({
+                    type: 'warning',
+                    message: '请输入验证码',
+                });
+            } else {
+                let param = {
+                    phone: this.getUserInfo.phone,
+                    smsCode: this.smsCode,
+                };
+                this.$axios('post', '/user/updatePhoneByApp', param).then(
+                    (res) => {
+                        if (res.code === 200) {
+                            this.$message({
+                                type: 'success',
+                                message: '绑定成功,请重新登录',
+                            });
+                            this.openPasswordDialog();
+                            this.loginOut();
+                        } else {
+                            this.$message({
+                                type: 'error',
+                                message: res.msg,
+                            });
+                        }
+                    },
+                );
+            }
+        },
+        // 修改密码
+        forgotPassword() {
+            if (this.smsCode === '') {
+                this.$message({
+                    type: 'warning',
+                    message: '请输入验证码',
+                });
+                return;
+            }
+            if (this.password === '') {
+                this.$message({
+                    type: 'warning',
+                    message: '请输入密码',
+                });
+                return;
+            }
+            let param = {
+                password: this.password,
+                phone: this.getUserInfo.phone,
+                smsCode: this.smsCode,
+            };
+            this.$axios('post', '/user/forgotPassword', param).then((res) => {
+                if (res.code === 200) {
+                    this.$message({
+                        type: 'success',
+                        message: '修改成功，请重新登录',
+                    });
+                    this.openPasswordDialog();
+                    this.loginOut();
+                } else {
+                    this.$message({
+                        type: 'error',
+                        message: res.msg,
+                    });
+                }
+            });
+        },
+        loginOut() {
+            this.$axios('post', '/user/loginOut').then((res) => {
+                if (res.code === 200) {
+                    Cookie.remove('token');
+                    this.$router.push('/');
+                }
+            });
         },
     },
 };
@@ -251,6 +464,28 @@ export default {
                     }
                 }
             }
+        }
+    }
+}
+.form-box {
+    width: 100%;
+    @include flexColumnStart();
+    padding: 0 90px;
+    .form-item {
+        width: 100%;
+        @include flexCenter();
+        margin: 14px 0;
+        .form-label {
+            white-space: nowrap;
+            display: block;
+            min-width: 56px;
+        }
+        .sms-btn {
+            margin-left: 30px;
+        }
+        .sub-btn {
+            width: 200px;
+            margin-top: 50px;
         }
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import * as modules from './modules';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 let store = new Vuex.Store({
     modules,
+    plugins: [createPersistedState()],
 });
 
 export default store;
